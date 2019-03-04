c1e4f545cf15316554727c5e6b7f53e212680f35
Marko Cupic <m.cupic@gmx.ch>
2019-03-04 17:02:22 +0100

    dev
    Upload

---
+++ src/MarkocupicContaoThemeBoilerplateBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright  Marko Cupic 2019 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    Contao Theme Boilerplate Bundle
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/markocupic/contao-theme-boilerplate-bundle
+ *
+ */
+namespace Markocupic\ContaoThemeBoilerplateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MarkocupicContaoThemeBoilerplateBundle
+ * @package Markocupic\ContaoThemeBoilerplateBundle
+ */
+class MarkocupicContaoThemeBoilerplateBundle extends Bundle
+{
+}
